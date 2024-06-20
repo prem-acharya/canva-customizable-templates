5d9b9a60a693c3a0eef3d95cfa5889b5af37f7a3
Prem Acharya <premacharya2193@gmail.com>
2024-06-20 18:46:12 +0530

    Fixed Canvas size and Added range slider

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Canvas({ background, text, fontSize, fontColor, fontFamily }) {
+  const [zoom, setZoom] = useState(50);
+
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="bg-no-repeat bg-center bg-cover transform scale-60 w-[210mm] h-[297mm] shadow-xl" style={{ backgroundImage: `url(${background})` }}>
+    <div className="flex justify-center items-center h-screen relative">
+      <div className="bg-no-repeat bg-center bg-cover w-[210mm] h-[297mm] shadow-xl" style={{ backgroundImage: `url(${background})`, transform: `scale(${zoom / 100})` }}>
         <div style={{ fontSize, color: fontColor, fontFamily }} contentEditable={true} suppressContentEditableWarning={true}>
           {/* Editing */}
         </div>
+      </div>
+      <div className="absolute bottom-0 right-0 p-4">
+        <input type="range" min="10" max="100" value={zoom} onChange={(e) => setZoom(e.target.value)} className="w-64" />
+        <span className="ml-2">{zoom}%</span>
       </div>
     </div>
   );
