2dbbf19caab92f347c20d41e32666f1de6ec06cf
Prem Acharya <premacharya2193@gmail.com>
2024-06-20 16:44:28 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function App() {
+
+  return (
+    <>
+      <div className="text-center text-3xl text-blue-600">canva customizable templates</div>
+    </>
+  )
+}
+
+export default App
